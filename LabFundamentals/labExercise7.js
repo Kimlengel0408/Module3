0a9c85f13e3036731a229525116190e2b3afe7bc
Kim Lengen Gesite <kimlengel0408@gmail.com>
2026-04-01 19:27:53 +1100

    Redone Lab 7

--- LabFundamentals/labExercise7.js
+++ LabFundamentals/labExercise7.js
@@ -1,5 +1,6 @@
 const basketballGame = {
   score: 0,
+  foul: 0,
   freeThrow() {
     this.score ++;
     return this;
@@ -12,9 +13,18 @@ const basketballGame = {
     this.score += 3;
     return this;
   },
-  halfTime() {
-    console.log("Halftime score is " + this.score);
+  foulCounter() {
+    this.foul ++;
+    return this;
   },
+  halfTime() {
+    console.log("Halftime score is " + this.score + ", and the number of fouls is " + this.foul);
+    return this;
+  },
+  fullTime(){
+    console.log(`Final score is ${this.score}! But the number of fouls is ${this.foul}`);
+    return this;
+  }
 };
 //modify each of the above object methods to enable function chaining as below:
 basketballGame
@@ -22,5 +32,9 @@ basketballGame
   .freeThrow()
   .freeThrow()
   .basket()
+  .foulCounter()
   .threePointer()
-  .halfTime();
+  .halfTime()
+  .foulCounter()
+  .foulCounter()
+  .fullTime();
