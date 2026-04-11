5eccc8f65dea080e9397f53c5d5fbf9080a466e3
Kim Lengen Gesite <kimlengel0408@gmail.com>
2026-04-11 11:05:25 +1000

    I think it is completely done

--- LabIntermediate/lab10.js
+++ LabIntermediate/lab10.js
@@ -1,3 +1,13 @@
 const today = new Date();
 console.log("Current time is " + today.toLocaleTimeString());
 console.log(today.getHours() + " hours have passed so far today");
+console.log(
+  today.getMinutes() +
+    today.getHours() * 60 +
+    " minutes have passed so far today",
+);
+console.log(
+  today.getSeconds() +
+    (today.getMinutes() + today.getHours() * 60) * 60 +
+    " seconds have passed so far today",
+);
