d65a6a52300ef7a3321d315510ddf9834b5fbe46
Kim Lengen Gesite <kimlengel0408@gmail.com>
2026-04-15 18:31:31 +1000

    Update 15/04

--- lectures.js
+++ lectures.js
@@ -1,3 +1,12 @@
-const mj = ['Michael', 'Jordan'];
-const [firstName, lastName, third] = mj;
-console.log(firstName, lastName, third);
+const meetup = {
+  title: "Strategy Conference",
+  participants: ["Chris", "Tina"],
+  date: "2023-06-01",
+};
+const meetupString = JSON.stringify(meetup); // convert object to string
+const meetupParsed = JSON.parse(meetupString, (key, value) => {
+  // convert string to object
+  if (!isNaN(Date.parse(value))) return new Date(value); // if valid date, create Date object
+  return value;
+});
+console.log(meetupParsed); // { title, participants: (as above), date: 2023-06-01T00:00:00.000Z }
